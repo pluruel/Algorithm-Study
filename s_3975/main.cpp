8bd20e85ca3662cc942cebf4cb077e0d5ae1d760
pluruel <pluruel@gmail.com>
2019-07-23 22:05:28 +0900

    승률 비교하기

--- s_3975/main.cpp
+++ s_3975/main.cpp
@@ -6,11 +6,28 @@ int main(int argc, const char * argv[]) {
     ios_base :: sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    short T;
+    int T;
     cin >> T;
-    
+    char ans[3][6] = {"ALICE", "BOB", "DRAW"};
     for (int z = 1; z <= T; ++z) {
-        cout << "#" << z << " " << "\n";
+        
+        double a_total, a_win, b_total, b_win;
+        cin >> a_win >> a_total >>b_win>> b_total;
+
+        a_win /= a_total;
+        b_win /= b_total;
+
+        int ansno;
+        if(a_win > b_win) {
+            ansno = 0;
+        } else if (b_win > a_win) {
+            ansno = 1;
+        } else {
+            ansno = 2;
+        }
+
+        cout << "#" << z << " " << ansno[ans] << "\n";
     }
     return 0;
 }
+
