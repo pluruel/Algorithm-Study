9a4e8ccb39b5e610915dd6ed188c78c642ea4fc8
pluruel <pluruel@gmail.com>
2019-08-06 16:47:54 +0900

    퇴사

---
+++ 백준/b_14499/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+int map[20][20];
+int dice[7] = {0,0,0,0,0,0,0};
+// int dice[7] = {0,1,2,3,4,5,6};
+int movens[6] = {2, 1, 5, 6, 2, 1};
+int movewe[6] = {4, 1, 3, 6, 4, 1};
+int mc[5] = {0, 0, 0, -1, 1};
+int mr[5] = {0, 1, -1, 0, 0};
+int colour[7];
+int N, M, x, y, K;
+
+int north() {
+    int temp[4];
+    for(int i = 0; i < 4; ++i)  temp[i] = dice[movens[i+2]];
+    for(int i = 0; i < 4; ++i) dice[movens[i+1]] = temp[i];
+    if(map[y][x] != 0)
+        dice[6] = map[y][x];
+    return dice[1];
+}
+
+int south() {
+    int temp[4];
+    for(int i = 0; i < 4; ++i)  temp[i] = dice[movens[i+1]];
+    for(int i = 0; i < 4; ++i) dice[movens[i+2]] = temp[i];
+    if(map[y][x] != 0)
+        dice[6] = map[y][x];
+    return dice[1];
+}
+
+int west() {
+    int temp[4];
+    for(int i = 0; i < 4; ++i)  temp[i] = dice[movewe[i+2]];
+    for(int i = 0; i < 4; ++i) dice[movewe[i+1]] = temp[i];
+    if(map[y][x] != 0)
+        dice[6] = map[y][x];
+    return dice[1];
+}
+
+int east() {
+    int temp[4];
+    for(int i = 0; i < 4; ++i)  temp[i] = dice[movewe[i+1]];
+    for(int i = 0; i < 4; ++i) dice[movewe[i+2]] = temp[i];
+    if(map[y][x] != 0)
+        dice[6] = map[y][x];
+    return dice[1];
+}
+
+void move(int way) {
+      x += mr[way]; y += mc[way];
+    if(x >= N || y >= N || x < 0 || y < 0) {
+        x -= mr[way]; y -= mc[way];
+        return;
+    }
+
+    switch(way) {
+        case 1:
+            cout << east() << "\n";
+            break;
+        case 2:
+            cout << west() << "\n";
+            break;
+        case 3:
+            cout << north() << "\n";
+            break;
+        case 4:
+            cout << south() << "\n";
+            break;
+    }
+    map[y][x] = 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    
+
+    cin >> N >> M >> x >> y >> K;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cin >> map[i][j];
+        }
+    }
+    int way;    
+
+    // east();
+
+    for (int i = 0; i < K; ++i) {
+        cin >> way;
+        move(way);
+    }
+}
