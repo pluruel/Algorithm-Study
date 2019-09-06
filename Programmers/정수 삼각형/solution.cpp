57c2f352fea90b3f4e3cc8cb1424480f9de66789
Junnoh Lee <pluruel@gmail.com>
2019-09-06 19:28:36 +0900

    정수 삼각형

--- Programmers/정수 삼각형/solution.cpp
+++ Programmers/정수 삼각형/solution.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
 
+    // DP의 가장 쉬운 예제
+    // 각 라인별로 가장 큰 값을 선택적으로 취하여 최종으로 보냄.
+    // 뒤에서 앞을 당겨오는 방식
     
     for(int i = 1; i < triangle.size(); ++i) {
         for (int j = 0; j < triangle[i].size(); ++j){
